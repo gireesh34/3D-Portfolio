50c272dfc0805f442be5d95ac6f0c0842bfeeeac
gireesh34 <152199178+gireesh34@users.noreply.github.com>
2024-10-11 00:34:57 +0530

    Update project files with latest changes

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,5 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
 import StarsCanvas from "./StarBackground";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
